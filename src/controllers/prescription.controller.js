99140ef1f68922bd8d79b596431dace56c2916b6
Emir Ali <emir-vi@hotmail.com>
2023-05-24 20:34:50 -0600

    Cambios 24-05-2023 08:34 p.m.

--- src/controllers/prescription.controller.js
+++ src/controllers/prescription.controller.js
@@ -13,6 +13,19 @@ export const getAllPrescription = async (req, res) => {
     }
 };
 
+export const getByPatient = async (req, res) => {
+    try {
+        const [rows] = await conn.query("SELECT R.id_receta, M.nombre as 'medico_nombre', M.apellido as 'medico_apellido', P.nombre as 'paciente_nombre', P.apellido as 'paciente_apellido', R.indicaciones, R.fecha_expedicion FROM Recetas R INNER JOIN Usuarios M ON R.id_medico = M.id_usuario INNER JOIN Usuarios P ON R.id_paciente = P.id_usuario WHERE P.id_usuario = ?", [req.params.id_paciente])
+        
+        res.json(rows);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Something goes wrong!'
+        })
+    }
+};
+
 export const createPrescription = async (req, res) => {
     
     const {id_medico, id_paciente, indicaciones, fecha_expedicion} = req.body
--- src/routes/prescription.routes.js
+++ src/routes/prescription.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
-import { getAllPrescription, createPrescription } from "../controllers/prescription.controller.js";
+import { getAllPrescription, createPrescription, getByPatient } from "../controllers/prescription.controller.js";
+import app from "../app.js";
 
 const router = Router();
 
 router.get('/prescription', getAllPrescription);
+router.get('/prescription/:id_paciente', getByPatient)
 router.post('/prescription', createPrescription);
 
 export default router;
